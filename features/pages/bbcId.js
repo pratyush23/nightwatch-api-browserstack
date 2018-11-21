5dc739981de55a8de9e087c7a30649f64144e8f3
Pratyush Sharma <sharmp09@mc-s103764.national.core.bbc.co.uk>
2018-11-21 15:37:45 +0000

    removed redundant code

--- features/pages/bbcId.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  elements: {
-    signInLinkViaOrb: { selector: '#idcta-statusbar' },
-    signInLinkViaUpsell: { selector: '.id5-cta.id4-cta-size-medium.id4-cta-color-mixed' },
-    emailInput: { selector: '#user-identifier-input' },
-    passwordInput: { selector: '#password-input' },
-    submitButton: { selector: '#submit-button' }
-  }
-};
--- features/step-definitions/my_sounds.js
+++ features/step-definitions/my_sounds.js
@@ -1,7 +1,6 @@
 const { client } = require('nightwatch-api');
 const { Given, Then, When } = require('cucumber');
 const mySounds = require('../pages/mySounds.js');
-const signInUsers = require('../support/signIn.js');
 
 Given(/^I am on the My Sounds page$/, () => {
   return client.url(mySounds.url());
@@ -13,13 +12,4 @@ Then(/^the title is "(.*?)"$/, (text) => {
 
 Then(/^the upsell title is "(.*?)"$/, (text) => {
   return client.expect.element(mySounds.elements.heading).text.to.equal(text);
-});
-
-When(/^I am signed in as "(.*?)" via upsell$/, (user) => {
-  return signInUsers.signInViaUpsell(user);
-
-});
-
-Then('I am taken to latest page', () => {
-  return client.expect.element(mySounds.elements.selectedsubNav).text.to.equal('Latest');
 });
